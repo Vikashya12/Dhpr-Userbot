197cf5cf32197ffc4611c55a62cbe973ae4a83b5
Vikky yadav <146602351+Vikashya12@users.noreply.github.com>
2024-05-18 15:50:08 +0530

    Update inline.py

--- AdityaHalder/modules/helpers/inline.py
+++ AdityaHalder/modules/helpers/inline.py
@@ -11,7 +11,7 @@ async def help_menu_logo(answer):
     if image:
         thumb_image = image
     else:
-        thumb_image = "https://telegra.ph/file/027283ee9defebc3298b8.png"
+        thumb_image = "https://telegra.ph/file/dce89ea79f06caa53d734.png"
     button = paginate_plugins(0, plugs, "help")
     answer.append(
         InlineQueryResultPhoto(
@@ -26,7 +26,7 @@ Genius Userbot » {__version__} ✨...
 Click On Below 🌺 Buttons To
 Get Userbot Commands.
 
-🌷Powered By : [Aditya Server](https://t.me/AdityaServer).**
+🌷Powered By : [DHPR](https://t.me/OWNER_DHPR).**
             """,
             reply_markup=InlineKeyboardMarkup(button),
         )
@@ -47,7 +47,7 @@ Genius Userbot » {__version__} ✨...
 Click On Below 🌺 Buttons To
 Get Userbot Commands.
 
-🌷Powered By : [Aditya Server](https://t.me/AdityaServer).**""",
+🌷Powered By : [DHPR](https://t.me/OWNER_DHPR).**""",
             disable_web_page_preview=True
             ),
             reply_markup=InlineKeyboardMarkup(button),
